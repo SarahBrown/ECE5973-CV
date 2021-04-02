80a85df5acb357a4ff2269f641db2bf4d5d2ebb3
SarahBrown <srb@ou.edu>
2021-04-01 23:37:17 -0500

    hw 3 so far

--- Final Project/camera3.py
+++ Final Project/camera3.py
@@ -5,32 +5,58 @@ import imutils
 def nothing(x):
     pass
 
-def detectShape(c):          #Function to determine type of polygon on basis of number of sides
+def detectShape(c):          
        shape = 'unknown' 
        peri=cv2.arcLength(c,True) 
        vertices = cv2.approxPolyDP(c, 0.02 * peri, True)
        sides = len(vertices) 
-       print(sides)
        if (sides == 3): 
-            shape='triangle' 
+            shape='d4' 
        elif(sides==4): 
              x,y,w,h=cv2.boundingRect(c)
              aspectratio=float(w)/h 
              if (aspectratio==1):
-                   shape='square'
+                   shape='d6'
              else:
-                   shape="rectangle" 
-       elif(sides==5):
-            shape='pentagon' 
-       elif(sides==6):
-            shape='hexagon' 
-       elif(sides==8): 
-            shape='octagon' 
-       elif(sides==9): 
-            shape='d12'
+                   shape="d6" 
+       elif(sides==7): 
+            shape='d12' 
        else:
            shape='circle' 
        return shape 
+
+def dieShape(img):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgNoise = cv2.medianBlur(imgGray,3)
+    imgBlur = cv2.GaussianBlur(imgNoise,(3,3),0)
+    imgCanny = cv2.Canny(imgBlur, 0, 255)
+
+    cnts = cv2.findContours(imgCanny.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    ret,thresh = cv2.threshold(imgCanny,200,225,1)
+    contours,h = cv2.findContours(thresh,1,2)
+
+    firstPass = img.copy()
+
+    for cnt in cnts:
+        if cv2.arcLength(cnt,False) > 0 and cv2.arcLength(cnt,False) < 300:
+            accuracy = 0.03*cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt, accuracy, True)
+
+            moment=cv2.moments(approx) 
+            if moment["m00"] != 0:
+                cx = int(moment['m10'] / moment['m00']) 
+                cy = int(moment['m01'] / moment['m00'])
+            else:
+                cx = 0
+                cy = 0
+            shape=detectShape(approx) 
+            cv2.drawContours(firstPass,[approx],-1,(0,255,0),2)
+            cv2.putText(firstPass,shape,(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)
+
+    return(firstPass)
 
 cap = cv2.VideoCapture(2)
 #cap.set(15,-4)
@@ -43,39 +69,12 @@ window_name='camera'
 cv2.namedWindow(window_name,cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(window_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
-lower = np.array([89,0,0])
-upper = np.array([179,255,255])
-
 while (True):
     ret, frame = cap.read()
 
-    imgGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    imgNoise = cv2.medianBlur(imgGray,3)
-    imgCanny = cv2.Canny(imgNoise, 0, 255)
+    frameDie = dieShape(frame)
 
-    cnts = cv2.findContours(imgCanny.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-
-    ret,thresh = cv2.threshold(imgCanny,200,225,1)
-    contours,h = cv2.findContours(thresh,1,2)
-
-    for c in cnts:
-        if cv2.arcLength(c,False) > 0 and cv2.arcLength(c,False) < 300:
-            accuracy = 0.03*cv2.arcLength(c,True)
-            approx = cv2.approxPolyDP(c, accuracy, True)
-
-            moment=cv2.moments(approx) 
-            if moment["m00"] != 0:
-                cx = int(moment['m10'] / moment['m00']) 
-                cy = int(moment['m01'] / moment['m00'])
-            else:
-                cx = 0
-                cy = 0
-            shape=detectShape(approx) 
-            cv2.drawContours(frame,[approx],-1,(0,255,0),2)
-            cv2.putText(frame,shape,(cx,cy),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),2)  #Putting name of polygon along with the shape 
-            cv2.imshow(window_name,frame) 
+    cv2.imshow(window_name,frameDie) 
 
     if cv2.waitKey(1) & 0xFF == ord('q'): # btw, you need to click the screen first. And then 
                                          # press q to quit
