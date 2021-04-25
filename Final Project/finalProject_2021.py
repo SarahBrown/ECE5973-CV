de85a67272ba1d3b88154e825f02fd716337c124
Sarah Brown <srb@ou.edu>
2021-04-25 01:49:54 -0500

    oops

--- Final Project/finalProject_2021.py
+++ Final Project/finalProject_2021.py
@@ -1,3 +1,164 @@
+import PySimpleGUI as sg
+
+import cv2
+
+import numpy as np
+
+
+def main():
+
+    sg.theme("LightGreen")
+
+
+    # Define the window layout
+
+    layout = [
+
+        [sg.Text("OpenCV Demo", size=(60, 1), justification="center")],
+
+        [sg.Image(filename="", key="-IMAGE-")],
+
+        [sg.Radio("None", "Radio", True, size=(10, 1))],
+
+        [
+
+            sg.Radio("threshold", "Radio", size=(10, 1), key="-THRESH-"),
+
+            sg.Slider(
+
+                (0, 255),
+
+                128,
+
+                1,
+
+                orientation="h",
+
+                size=(40, 15),
+
+                key="-THRESH SLIDER-",
+
+            ),
+
+        ],
+
+        [
+
+            sg.Radio("canny", "Radio", size=(10, 1), key="-CANNY-"),
+
+            sg.Slider(
+
+                (0, 255),
+
+                128,
+
+                1,
+
+                orientation="h",
+
+                size=(20, 15),
+
+                key="-CANNY SLIDER A-",
+
+            ),
+
+            sg.Slider(
+
+                (0, 255),
+
+                128,
+
+                1,
+
+                orientation="h",
+
+                size=(20, 15),
+
+                key="-CANNY SLIDER B-",
+
+            ),
+
+        ],
+
+        [
+
+            sg.Radio("blur", "Radio", size=(10, 1), key="-BLUR-"),
+
+            sg.Slider(
+
+                (1, 11),
+
+                1,
+
+                1,
+
+                orientation="h",
+
+                size=(40, 15),
+
+                key="-BLUR SLIDER-",
+
+            ),
+
+        ],
+
+        [
+
+            sg.Radio("hue", "Radio", size=(10, 1), key="-HUE-"),
+
+            sg.Slider(
+
+                (0, 225),
+
+                0,
+
+                1,
+
+                orientation="h",
+
+                size=(40, 15),
+
+                key="-HUE SLIDER-",
+
+            ),
+
+        ],
+
+        [
+
+            sg.Radio("enhance", "Radio", size=(10, 1), key="-ENHANCE-"),
+
+            sg.Slider(
+
+                (1, 255),
+
+                128,
+
+                1,
+
+                orientation="h",
+
+                size=(40, 15),
+
+                key="-ENHANCE SLIDER-",
+
+            ),
+
+        ],
+
+        [sg.Button("Exit", size=(10, 1))],
+
+    ]
+
+
+    # Create the window and show it without the plot
+
+    window = sg.Window("OpenCV Integration", layout, location=(800, 400))
+
+
+    cap = cv2.VideoCapture(0)
+
+
     while True:
 
         event, values = window.read(timeout=20)
