012e05967769278f9650fa9d2f1d7b696d5c1ebd
Sarah Brown <srb@ou.edu>
2021-04-25 01:48:40 -0500

    sample code for pysimplegui

--- Final Project/finalProject_2021.py
+++ Final Project/finalProject_2021.py
@@ -1,12 +1,64 @@
-from cv2 import cv2
-import numpy as np
-import imutils
-import pytesseract
-import PySimpleGUI as sg
+    while True:
 
-config = '--oem 3 --psm 6 outputbase digits'
+        event, values = window.read(timeout=20)
 
-def nothing(x):
-    pass
+        if event == "Exit" or event == sg.WIN_CLOSED:
 
-sg.Window(title="Hello World", layout=[[]], margins=(100,50)).read()
+            break
+
+
+        ret, frame = cap.read()
+
+
+        if values["-THRESH-"]:
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)[:, :, 0]
+
+            frame = cv2.threshold(
+
+                frame, values["-THRESH SLIDER-"], 255, cv2.THRESH_BINARY
+
+            )[1]
+
+        elif values["-CANNY-"]:
+
+            frame = cv2.Canny(
+
+                frame, values["-CANNY SLIDER A-"], values["-CANNY SLIDER B-"]
+
+            )
+
+        elif values["-BLUR-"]:
+
+            frame = cv2.GaussianBlur(frame, (21, 21), values["-BLUR SLIDER-"])
+
+        elif values["-HUE-"]:
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            frame[:, :, 0] += int(values["-HUE SLIDER-"])
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+
+        elif values["-ENHANCE-"]:
+
+            enh_val = values["-ENHANCE SLIDER-"] / 40
+
+            clahe = cv2.createCLAHE(clipLimit=enh_val, tileGridSize=(8, 8))
+
+            lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+
+            lab[:, :, 0] = clahe.apply(lab[:, :, 0])
+
+            frame = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+
+        imgbytes = cv2.imencode(".png", frame)[1].tobytes()
+
+        window["-IMAGE-"].update(data=imgbytes)
+
+
+    window.close()
+
+
+main()
